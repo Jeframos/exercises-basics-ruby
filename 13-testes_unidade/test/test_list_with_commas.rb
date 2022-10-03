e13dd673a26b42feefcccc393af15075025676e4
Jefferson Silva Ramos <jefferson.ramos.ext@sodexo.com>
2022-10-02 23:46:45 -0300

    Chore: Evolucao testes unidade

--- 13-testes_unidade/test/test_list_with_commas.rb
+++ 13-testes_unidade/test/test_list_with_commas.rb
@@ -3,15 +3,22 @@ require 'list_with_commas'
 
 
 class TestListWithCommas < Minitest::Test
+
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = ['pear','apple']
-        assert('pear and apple' == list.join)
+        assert_equal('pear and apple', list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = ['pear', 'orange','apple']
-        assert('pear, orange, and apple' == list.join)
+        assert_equal('pear, orange, and apple', list.join)
+    end
+
+    def test_it_prints_one_word_alone
+        list = ListWithCommas.new
+        list.items = ['apple']
+        assert_equal('apple', list.join)
     end
 end
