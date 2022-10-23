8f255437151be1c1a98bbe615f48a81f04a474ca
Jefferson Silva Ramos <jefferson.ramos.ext@sodexo.com>
2022-10-23 00:21:23 -0300

    Chore: Melhoria aplicacao

---
+++ 14-aplicativos_web/movies/lib/movie_store.rb
@@ -0,0 +1,18 @@
+require 'yaml/store'
+
+class MovieStore
+
+    def initialize(file_name)  #O initialize vai buscar o nome do arquivo que será criado
+        @store = YAML::Store.new(file_name)
+    end
+
+    def save(movie)
+        @store.transaction do
+            unless movie.id
+                highest_id = @store.roots.max || 0  #Se o "@store.roots.max" estiver nil, então deve virar o valor 0.
+                movie.id = highest_id + 1
+            end
+            @store[movie.id] = movie
+        end
+    end
+end
